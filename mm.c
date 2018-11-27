8dbede9e9bf0d5bf49db7f29814ff66a99b258e0
liuchao <380263137@qq.com>
2018-11-27 21:01:52 +0800

    fix

--- mm.c
+++ mm.c
@@ -64,6 +64,9 @@ team_t team =
 #define SET_NEXT_FREE(now_block, next_block) {size_t ** tmp = (size_t **) GET_NEXT_FREE_ADDR(now_block); *tmp = (size_t *)next_block;}
 #define SET_PREV_FREE(now_block, prev_block) {size_t ** tmp = (size_t **) GET_PREV_FREE_ADDR(now_block); *tmp = (size_t *)prev_block;}
 
+
+/* ---------------------------------------Debug------------------------------------------- */
+
 #define DEBUG 1
 int c = 0;
 
@@ -75,7 +78,7 @@ void DISP_PROGRESS()
         printf("\r%d", c);
     }
 
-    if (c == 209)
+    if (c == 212)
     {
         int a = 1;
     }
@@ -84,6 +87,10 @@ void DISP_PROGRESS()
     { printf("\r-----completed-----\n"); }
     fflush(stdout);
 }
+
+
+
+/* -----------------------------------list------------------------------------------------ */
 
 typedef struct _list
 {
@@ -101,23 +108,87 @@ void list_init(List *l, size_t min, size_t max)
     l->cnt = 0;
 }
 
+/*
+ * remove block from list, return next block in free_list
+ * */
+void * list_remove(List *free_list, void * block)
+{
+    size_t *prev_free = GET_PREV_FREE(block);
+    size_t *next_free = GET_NEXT_FREE(block);
+
+    if (NULL != prev_free)
+    {
+        SET_NEXT_FREE(prev_free, next_free);
+    }
+
+    if (NULL != next_free)
+    {
+        SET_PREV_FREE(next_free, prev_free);
+    }
+
+    if (free_list->head == block)
+    {
+        free_list->head = (char *) next_free;
+    }
+
+    free_list->cnt -= 1;
+
+    return next_free;
+}
+
+/*
+ * insert block after prev_block, if prev_block is NULL, equal to push_head
+ * */
+void list_insert_after(List *free_list, void *prev_block, void *block)
+{
+    if (prev_block == NULL)
+    {
+        // insert head
+        SET_NEXT_FREE(block, free_list->head);
+        SET_PREV_FREE(block, NULL);
+        free_list->head = block;
+
+        if (0 != free_list->cnt)
+        {
+            SET_PREV_FREE(free_list->head, block);
+        }
+    }
+    else
+    {
+        // insert after prev_block
+        void * next_free = GET_NEXT_FREE(prev_block);
+        SET_NEXT_FREE(block, next_free);
+        SET_PREV_FREE(block, prev_block);
+        SET_PREV_FREE(next_free, block);
+        SET_NEXT_FREE(prev_block, block);
+    }
+
+    free_list->cnt += 1;
+}
+
 List g_free_list;
 
-void *find_first_fit(List *free_list, size_t new_size);
 
-void *find_best_fit(List *free_list, size_t new_size);
+/* -----------------------------------------tools---------------------------------------- */
 
-void push_lifo(List *free_list, void *block);
+void * get_new_block(size_t new_size);
 
-void push_ao(List *free_list, void *block);
+void * split(List *free_list, void * block, size_t new_size);
 
-void merge_lifo(void *block);
+void *get_first_fit(List *free_list, size_t new_size);
 
-void merge_ao(void *block);
+void *get_best_fit(List *free_list, size_t new_size);
+
+void lifo_free(List *free_list, void *block);
+
+void ao_free(List *free_list, void *block);
 
 void *_mm_malloc(List *free_list, size_t new_size, void *(*get_block)(List *, size_t));
 
-void _mm_free(void *block, void (*insert)(List *, void *), void (*merge)(void *));
+void _mm_free(void *block, void (*free)(List *, void *));
+
+
+/* -----------------------------------------malloc---------------------------------------- */
 
 /*
  * mm_init - initialize the malloc package.
@@ -138,7 +209,7 @@ void *mm_malloc(size_t free_size)
 
     size_t new_size = ALIGN(free_size + SIZE_T_SIZE);
 
-    return _mm_malloc(&g_free_list, new_size, find_first_fit);
+    return _mm_malloc(&g_free_list, new_size, get_first_fit);
 }
 
 /*
@@ -150,7 +221,7 @@ void mm_free(void *ptr)
 
     void *now_block = (void *) ((char *) ptr - SIZE_T_SIZE);
 
-    _mm_free(now_block, push_lifo, merge_lifo);
+    _mm_free(now_block, lifo_free);
 }
 
 /*
@@ -203,90 +274,30 @@ void *_mm_malloc(List *free_list, size_t new_size, void *(*get_block)(List *, si
         return NULL;
     }
 
-    // Set next block's indicators
-    if (NEXT_BLOCK(now_block) <= mem_heap_hi())
-    {
-        SET_LAST_USED(NEXT_BLOCK(now_block));
-        SET_LAST_USED(NEXT_BLOCK_TAIL(now_block));
-    }
-
-    // Block is too large to alloc totally
-    if (BLOCK_SIZE(now_block) - new_size >= 4 * sizeof(size_t))
-    {
-        size_t remain = BLOCK_SIZE(now_block) - new_size;
-        void *new_block = (char *) now_block + new_size;
-
-        SET_SIZE(now_block, remain);
-        *(size_t * )BLOCK_TAIL(now_block) = *(size_t *) now_block;
-        // Remain block need not change its position in global free list
-        // But it need be added into list if it is new generated.
-
-        SET_SIZE(new_block, new_size);
-        CLEAR_LAST_USED(new_block);
-        SET_USED(new_block);
-
-    }
-    else
-    {
-        size_t *prev_free = GET_PREV_FREE(now_block);
-        size_t *next_free = GET_NEXT_FREE(now_block);
-
-        if (NULL != prev_free)
-        {
-            SET_NEXT_FREE(prev_free, next_free);
-        }
-
-        if (NULL != next_free)
-        {
-            SET_PREV_FREE(next_free, prev_free);
-        }
-
-        if (free_list->head == now_block)
-        {
-            free_list->head == GET_NEXT_FREE(now_block);
-        }
-
-        SET_USED(now_block);
-    }
-
     return AVAILABLE(now_block);
 }
 
-void _mm_free(void *block, void (*insert)(List *, void *), void (*merge)(void *))
+void _mm_free(void *block, void (*free)(List *, void *))
 {
-    insert(&g_free_list, block);
-    merge(block);
+    free(&g_free_list, block);
 }
 
-void *find_first_fit(List *free_list, size_t new_size)
+void *get_first_fit(List *free_list, size_t new_size)
 {
     void *now_block = NULL;
     for (now_block = free_list->head; now_block != NULL; now_block = GET_NEXT_FREE(now_block))
     {
         if (BLOCK_SIZE(now_block) >= new_size)
         {
-            return now_block;
+            return split(free_list, now_block, new_size);
         }
     }
 
-    now_block = (char *) mem_heap_hi() + 1;
-
-    if (mem_sbrk(new_size) == (void *) -1)
-    {
-        return NULL;
-    }
-    else
-    {
-        SET_SIZE(now_block, new_size);
-        CLEAR_USED(now_block);
-        CLEAR_LAST_USED(now_block);
-        SET_PREV_FREE(now_block, NULL);
-        SET_NEXT_FREE(now_block, NULL);
-        return now_block;
-    }
+    // Cannot find fit block.
+    return get_new_block(new_size);
 }
 
-void *find_best_fit(List *free_list, size_t new_size)
+void *get_best_fit(List *free_list, size_t new_size)
 {
     size_t min_diff = (size_t) - 1;
     void *min_diff_block = NULL;
@@ -297,6 +308,7 @@ void *find_best_fit(List *free_list, size_t new_size)
         diff = BLOCK_SIZE(now_block) - new_size;
         if (0 == diff)
         {
+            list_remove(free_list, now_block);
             return now_block;
         }
         else if (0 < diff && min_diff > diff)
@@ -306,205 +318,131 @@ void *find_best_fit(List *free_list, size_t new_size)
         }
     }
 
-
     if (min_diff_block != NULL)
     {
-        return min_diff_block;
+        return split(free_list, min_diff_block, new_size);
     }
     else
     {
-        min_diff_block = (char *) mem_heap_hi() + 1;
+        return get_new_block(new_size);
+    }
+}
 
-        if (mem_sbrk(new_size) == (void *) -1)
+void lifo_free(List *free_list, void *new_block)
+{
+    void * ultimate_block = new_block;                                  // merge blocks to there
+    int total_size = BLOCK_SIZE(new_block);                             // totally merge size
+    void * next_block = (char *) new_block + BLOCK_SIZE(new_block);     // new_block can merge with this block
+
+    for (void *old_block = free_list->head; old_block != NULL; )
+    {
+        if (old_block == next_block)
         {
-            return NULL;
+            // some block can merge after new_block
+            total_size += BLOCK_SIZE(old_block);
+            old_block = list_remove(free_list, old_block);
         }
         else
         {
-            SET_SIZE(min_diff_block, new_size);
-            CLEAR_USED(min_diff_block);
-            CLEAR_LAST_USED(min_diff_block);
-            SET_PREV_FREE(min_diff_block, NULL);
-            SET_NEXT_FREE(min_diff_block, NULL);
-            return min_diff_block;
-        }
-    }
-}
-
-void push_lifo(List *free_list, void *block)
-{
-    assert(free_list != NULL);
-
-    if (block > mem_heap_lo() && 0 == GET_LAST_USED(block))
-    {
-        // able to merge with preview block, do nothing
-
-    }
-    else
-    {
-        SET_NEXT_FREE(block, free_list->head);
-        SET_PREV_FREE(block, NULL);
-        free_list->head = block;
-
-        if (0 != free_list->cnt)
-        {
-            SET_PREV_FREE(free_list->head, block);
-        }
-
-
-        free_list->cnt += 1;
-    }
-}
-
-void merge_lifo(void *block)
-{
-    void *prev_free = NULL;
-    void *next_free = NULL;
-    void *now_block = block;
-    size_t free_size = BLOCK_SIZE(now_block);
-
-    if (now_block > mem_heap_lo() && 0 == GET_LAST_USED(now_block))
-    {
-        // able to merge with prev block
-        free_size += BLOCK_SIZE(PREV_BLOCK(now_block));
-        now_block = PREV_BLOCK(now_block);
-
-        SET_SIZE(now_block, free_size);
-        CLEAR_USED(now_block);
-
-        *(size_t * )BLOCK_TAIL(now_block) = *(size_t *) now_block;
-    }
-    else if (NEXT_BLOCK(now_block) <= mem_heap_hi() && 0 == GET_USED(NEXT_BLOCK(now_block)))
-    {
-        //able to merge with next block
-
-        // delete next block in list
-        now_block = NEXT_BLOCK(now_block);
-        prev_free = GET_PREV_FREE(now_block);
-        next_free = GET_NEXT_FREE(now_block);
-
-        if (prev_free != NULL)
-        {
-            SET_NEXT_FREE(prev_free, next_free);
-        }
-
-        if (next_free != NULL)
-        {
-            SET_PREV_FREE(next_free, prev_free);
-        }
-
-        free_size += BLOCK_SIZE(now_block);
-        now_block = block;
-
-        SET_SIZE(now_block, free_size);
-        CLEAR_USED(now_block);
-
-        *(size_t * )BLOCK_TAIL(now_block) = *(size_t *) now_block;
-    }
-
-    if (NEXT_BLOCK(now_block) <= mem_heap_hi())
-    {
-        CLEAR_LAST_USED(NEXT_BLOCK(now_block));
-        CLEAR_LAST_USED(NEXT_BLOCK_TAIL(now_block));
-    }
-}
-
-void push_ao(List *free_list, void *block)
-{
-    assert(free_list != NULL);
-
-    if (free_list->cnt == 0 || free_list->head > (char *) block)
-    {
-        // insert head
-        SET_PREV_FREE(block, NULL);
-        SET_NEXT_FREE(block, free_list->head);
-        free_list->head = (char *) block;
-    }
-    else
-    {
-        size_t *now_block = NULL;
-        size_t *prev_block = (size_t *) free_list->head;
-
-        for (now_block = (size_t * )GET_NEXT_FREE(free_list->head);
-             now_block != NULL; now_block = GET_NEXT_FREE(now_block))
-        {
-            if (now_block > (size_t *) block)
+            if ((char *) old_block + BLOCK_SIZE(old_block) == new_block)
             {
-                //insert before now_block
-                SET_NEXT_FREE(block, now_block);
-                SET_PREV_FREE(block, prev_block);
-                SET_PREV_FREE(now_block, block);
-                SET_NEXT_FREE(prev_block, block);
-                prev_block = NULL;
-                break;
+                // new_block can merge with before
+                ultimate_block = old_block;
+                total_size += BLOCK_SIZE(old_block);
             }
 
-            prev_block = now_block;
+            old_block = GET_NEXT_FREE(old_block);
         }
-
-        if (NULL != prev_block)
-        {
-            //insert tail;
-            SET_NEXT_FREE(prev_block, block);
-            SET_PREV_FREE(block, prev_block);
-        }
-
     }
 
-    free_list->cnt += 1;
+    SET_SIZE(ultimate_block, total_size);
+
+    if (ultimate_block == new_block)
+    {
+        list_insert_after(free_list, NULL, new_block);
+    }
 }
 
-void merge_ao(void *block)
+void ao_free(List *free_list, void *new_block)
 {
-    void *prev_free = NULL;
-    void *next_free = NULL;
-    void *now_block = block;
-    size_t free_size = BLOCK_SIZE(now_block);
+    void * prev_block = NULL;
+    void * ultimate_block = new_block;
+    int total_size = BLOCK_SIZE(new_block);
+    void * next_block = (char *) new_block + BLOCK_SIZE(new_block);
 
-    if (NEXT_BLOCK(now_block) <= mem_heap_hi() && 0 == GET_USED(NEXT_BLOCK(now_block)))
+    for (void *old_block = free_list->head; old_block != NULL; )
     {
-        // able to merge with next block
-        next_free = GET_NEXT_FREE(NEXT_BLOCK(now_block));
-        free_size += BLOCK_SIZE(NEXT_BLOCK(now_block));
-    }
-    else
-    {
-        next_free = GET_NEXT_FREE(now_block);
-        if (NEXT_BLOCK(now_block) <= mem_heap_hi())
+        if (old_block >= next_block)
         {
-            CLEAR_LAST_USED(NEXT_BLOCK(now_block));
-            CLEAR_LAST_USED(NEXT_BLOCK_TAIL(now_block));
+            // test whether merge or not
+            if (old_block == next_block)
+            {
+                total_size += BLOCK_SIZE(old_block);
+                old_block = list_remove(free_list, old_block);
+            }
+            break;
+        }
+        else
+        {
+            // if can merge, do not need insert.
+            if ((char *) old_block + BLOCK_SIZE(old_block) == new_block)
+            {
+                ultimate_block = old_block;
+                total_size += BLOCK_SIZE(old_block);
+            }
+
+            else if (prev_block == NULL && GET_NEXT_FREE(old_block) > new_block)
+            {
+                prev_block = old_block;
+            }
+
+            old_block = GET_NEXT_FREE(old_block);
         }
     }
 
-    if (now_block > mem_heap_lo() && 0 == GET_LAST_USED(now_block))
+    SET_SIZE(ultimate_block, total_size);
+
+    if (ultimate_block == new_block)
     {
-        // able to merge with preview block
-        prev_free = GET_PREV_FREE(PREV_BLOCK(now_block));
-        free_size += BLOCK_SIZE(PREV_BLOCK(now_block));
-        now_block = PREV_BLOCK(now_block);
+        list_insert_after(free_list, prev_block, new_block);
+    }
+}
+
+void * get_new_block(size_t new_size)
+{
+    void * block = (char *) mem_heap_hi() + 1;
+
+    if (mem_sbrk(new_size) == (void *) -1)
+    {
+        return NULL;
     }
     else
     {
-        prev_free = GET_PREV_FREE(now_block);
+        SET_SIZE(block, new_size);
+        return block;
     }
+}
 
-    // merge block, modify list
-    SET_SIZE(now_block, free_size);
-    CLEAR_USED(now_block);
-
-    *(size_t * )BLOCK_TAIL(now_block) = *(size_t *) now_block;
-
-    SET_NEXT_FREE(now_block, next_free);
-    SET_PREV_FREE(now_block, prev_free);
-
-    if (prev_free != NULL)
+/*
+ * Split block by new size, return suitable block
+ * */
+void * split(List *free_list, void * block, size_t new_size)
+{
+    // Block is too large
+    if (BLOCK_SIZE(block) - new_size >= 4 * sizeof(size_t))
     {
-        SET_NEXT_FREE(prev_free, now_block);
+        size_t remain = BLOCK_SIZE(block) - new_size;
+        SET_SIZE(block, remain);
+        // Remain block need not be removed
+
+        void *new_block = (void *) ((char *) block + new_size);
+        SET_SIZE(new_block, new_size);
+
+        return new_block;
     }
-
-    if (next_free != NULL)
+    else
     {
-        SET_PREV_FREE(next_free, now_block);
+        list_remove(free_list, block);
+        return block;
     }
 }
